b9b57e219edbe9dfbc9e39e6d09367717cc3376b
Deepak Singh <singhdeepak5219@gmail.com>
2022-12-11 22:19:04 +0530

    testing route

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ require('dotenv').config();
 require('./config/database').connect();
 const express = require('express');
 const app = express();
+const sample = require('./models/sample');
+
+
+app.post('/sample', async (req,res) => {
+  const sm =  await sample.create({
+        name:"Deepak Singh"
+    })
+
+    res.status(200).json(sm);
+
+})
 
 
 
